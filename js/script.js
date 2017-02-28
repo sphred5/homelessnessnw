8855d24d237cbca9b7a99d9c46c4156962dfaa49
billoosijok <billoosijok@gmail.com>
2017-02-28 14:44:46 -0800

    animation is better now

--- js/script.js
+++ js/script.js
@@ -15,34 +15,5 @@ window.addEventListener('load', function () {
 			sidebar.style['top'] =  '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
 		});
 	}
-
-	// var cards = document.querySelectorAll('.card');
-
-	// for (var i = 0; i < cards.length; i++) {
-		
-	// 	card.style.top = '100px';
-	// 	card.style.opacity = 0;
-
-	// 	setTimeout(function() {
-	// 		animate(card, 'top', '0px');
-	// 		animate(card, 'opacity', '1');
-	// 	});
-		
-	// }
-	var cards = $('.card');
-	cards.css({
-		position: 'relative',
-		opacity: '0',
-		top: '100px'
-	});
-
-	cards.each(function(index, el) {
-		// console.log($(this));
-		$(this).delay(index*200).animate({
-			top: 0,
-			opacity: 1}, 400);
-	});
-	
-
 });
 	
