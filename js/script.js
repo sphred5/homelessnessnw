c07fe08e25150e9c7357f8813359c01a7dba3aa3
billoosijok <billoosijok@gmail.com>
2017-02-28 11:24:10 -0800

    website is responsive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 window.addEventListener('load', function () {
-	var background = document.getElementById('bg');
-	var backgroundCords = background.getBoundingClientRect();
-	
-	var sidebar = document.getElementById('sidebar');
-	var sidebarCords = sidebar.getBoundingClientRect();
+	var mediaQuery = 700;
+	console.log(document.documentElement.clientWidth);
+	if(Math.max(window.innerWidth, document.documentElement.clientWidth) > mediaQuery) {
+		var background = document.getElementById('bg');
+		var backgroundCords = background.getBoundingClientRect();
+		
+		var sidebar = document.getElementById('sidebar');
+		var sidebarCords = sidebar.getBoundingClientRect();
 
-	var num = backgroundCords.height;
-	window.addEventListener('scroll', function() {
-		background.style['top'] = '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
+		var num = backgroundCords.height;
+		window.addEventListener('scroll', function() {
+			background.style['top'] = '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
+		
+			sidebar.style['top'] =  '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
+		});
+	}
 	
-		sidebar.style['top'] =  '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
-	});
 
 });
 	
