e3482571b66a887f7c74060619f86ce445505572
billoosijok <billoosijok@gmail.com>
2017-02-28 12:31:45 -0800

    animated intrance

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
 	var mediaQuery = 700;
-	console.log(document.documentElement.clientWidth);
+
 	if(Math.max(window.innerWidth, document.documentElement.clientWidth) > mediaQuery) {
 		var background = document.getElementById('bg');
 		var backgroundCords = background.getBoundingClientRect();
@@ -15,6 +15,20 @@ window.addEventListener('load', function () {
 			sidebar.style['top'] =  '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
 		});
 	}
+
+	var cards = $('.card');
+	cards.css({
+		position: 'relative',
+		opacity: '0',
+		top: '100px'
+	});
+
+	cards.each(function(index, el) {
+		console.log($(this));
+		$(this).delay(index*100).animate({
+			top: 0,
+			opacity: 1}, 200);
+	});
 	
 
 });
