bb9ceb6a8bc53258eb5176201d0c5fc14969bf1c
Steven Haye <sphred5@gmail.com>
2017-02-28 20:20:51 -0800

    added about page

--- js/script.js
+++ js/script.js
@@ -4,16 +4,14 @@ window.addEventListener('load', function () {
 	if(Math.max(window.innerWidth, document.documentElement.clientWidth) > mediaQuery) {
 		var background = document.getElementById('bg');
 		var backgroundCords = background.getBoundingClientRect();
-		
+
 		var sidebar = document.getElementById('sidebar');
 		var sidebarCords = sidebar.getBoundingClientRect();
 
 		var num = backgroundCords.height;
 		window.addEventListener('scroll', function() {
 			background.style['top'] = '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
-		
 			sidebar.style['top'] =  '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
 		});
 	}
 });
-	
