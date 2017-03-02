0c1893e1c57ecc4813b6c8cbac97091a04c5e1c3
billoosijok <billoosijok@gmail.com>
2017-03-01 23:51:12 -0800

    populated content

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 window.addEventListener('load', function () {
 	var mediaQuery = 700;
 
-	if(Math.max(window.innerWidth, document.documentElement.clientWidth) > mediaQuery) {
-		var background = document.getElementById('bg');
-		var backgroundCords = background.getBoundingClientRect();
+	var background = document.getElementById('bg');
+	var backgroundCords = background.getBoundingClientRect();
 
-		var sidebar = document.getElementById('sidebar');
-		var sidebarCords = sidebar.getBoundingClientRect();
+	var sidebar = document.getElementById('sidebar');
+	var sidebarCords = sidebar.getBoundingClientRect();
 
-		var num = backgroundCords.height;
-		window.addEventListener('scroll', function() {
-			background.style['top'] = '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
-			sidebar.style['top'] =  '-' + Math.min(200, pageYOffset/(backgroundCords.height)*100) + 'px';
-		});
-	}
+	var num = backgroundCords.height;
+	window.addEventListener('scroll', function() {
+		if(Math.max(window.innerWidth, document.documentElement.clientWidth) > mediaQuery) {
+			background.style['top'] = '-' + Math.min(150, pageYOffset/(backgroundCords.height)*100) + 'px';
+			sidebar.style['top'] =  '-' + Math.min(150, pageYOffset/(backgroundCords.height)*100) + 'px';
+		} else {
+			background.style['top'] = '0px';
+			sidebar.style['top'] = '0px';
+		}
+	});
+	
 });
